ac6b006d4f131e7abe34fea6b9c5aba91c8fbe71
pjirvine <p.j.irvine@gmail.com>
2021-06-18 18:02:56 +0100

    added better_worse and multi-panel maps

--- analysis.py
+++ analysis.py
@@ -34,3 +34,115 @@ def ttest_sub(mean_1, std_1, nyears_1, mean_2, std_2, nyears_2, equal_var=True):
 
     return pvalue_out
 
+"""
+###
+Define functions which determine better off, worse off, don't know and all sub-types
+###
+"""
+
+def bools_x8_3ttests(test_1,test_2,test_3):
+    
+    """
+    This function produces booleans for all 8 combinations of the three input T-tests:
+    test_1 = abs(SRM_anom), abs(CO2_anom), SRM_std, CO2_std
+    test_2 = CO2, ctrl
+    test_3 = SRM, ctrl
+    """
+    
+    return {'FFF': np.logical_not(test_1) * np.logical_not(test_2) * np.logical_not(test_3),
+            'FFT': np.logical_not(test_1) * np.logical_not(test_2) * (test_3),
+            'FTF': np.logical_not(test_1) * (test_2) * np.logical_not(test_3),
+            'FTT': np.logical_not(test_1) * (test_2) * (test_3),
+            'TFF': (test_1) * np.logical_not(test_2) * np.logical_not(test_3),
+            'TFT': (test_1) * np.logical_not(test_2) * (test_3),
+            'TTF': (test_1) * (test_2) * np.logical_not(test_3),
+            'TTT': (test_1) * (test_2) * (test_3),}
+
+# This snippet allows keys + values in dictionaries to be read as variables of form X.key instead of D['key']
+class Bunch(object):
+    def __init__(self, adict):
+        self.__dict__.update(adict)
+
+def types_groups_from_bools(bool_dict, ratio):
+    
+    """
+    This function takes the output from bools_x8_3ttests and the ratio and returns them with standard names and in groups
+    
+    See hypothesis document for details.
+    
+    ratio = SRM_anom / CO2_anom
+    |>1| = exacerbated
+    |<1| = moderated
+    +ve = same sign
+    -ve = reversed sign
+    """
+    
+    """
+        1: SRM vs CO2,   2: CO2 vs CTRL,   3: SRM vs CTRL
+    """
+    type_dict = {'A1': bool_dict['TFF'],
+                 'A2': bool_dict['TFT'],
+                 'A3': bool_dict['TTF'],
+                 'A4': bool_dict['TTT'],
+                 'B1': bool_dict['FFF'],
+                 'B2': bool_dict['FFT'],
+                 'B3': bool_dict['FTF'],
+                 'B4': bool_dict['FTT'],}
+
+    td = Bunch(type_dict)
+    
+    group_dict = {'all': td.A1 + td.A2 + td.A3 + td.A4 + td.B1 + td.B2 + td.B3 + td.B4,
+                  'dont_know': td.A1 + td.B1 + td.B2 + td.B3 + td.B4,
+                  'better_off': td.A3 + td.A4 * (abs(ratio) < 1),
+                  'worse_off': td.A2 + td.A4 * (abs(ratio) > 1),
+                  'not_small': td.A2 + td.A3 + td.A4 + td.B4,
+                  'certain': td.A2 + td.A3 + td.A4,
+                  'dont_know_small': td.A1 + td.B1 + td.B2 + td.B3,
+                  'dont_know_big_none': td.B4 * (ratio > 0),
+                  'dont_know_big_over': td.B4 * (ratio < 0),
+                  'better_off_perfect': td.A3,
+                  'better_off_under': td.A4 * (0 < ratio) * (ratio < 1),
+                  'better_off_over': td.A4 * (-1 < ratio) * (ratio < 0),
+                  'worse_off_novel': td.A2,
+                  'worse_off_exacerbate': td.A4 * (ratio > 1),
+                  'worse_off_too_much': td.A4 * (ratio < -1),
+                  'all_over': (td.A4 + td.B4) * (ratio < 0),
+                 }
+
+    return type_dict, group_dict
+
+"""
+This function calculates the fraction which are better, worse and don't know
+"""
+
+def better_worse_off(SRM_mean, SRM_std, CO2_mean, CO2_std, CTRL_mean, CTRL_std, nyears, ttest_level):
+    
+    # define anomalies
+    CO2_anom = CO2_mean - CTRL_mean
+    SRM_anom = SRM_mean - CTRL_mean
+
+    # ratio of anomalies
+    try: # check for divide by zero error and create very big number instead
+        ratio = SRM_anom / CO2_anom
+    except ZeroDivisionError:
+        ratio = np.sign(SRM_anom) * 9.999*10**99
+
+    # absolute_double_anom T-Test
+    ttest_1 = ttest_sub(abs(SRM_anom), SRM_std, nyears,
+                        abs(CO2_anom), CO2_std, nyears) < ttest_level
+    # CO2, ctrl T-Test
+    ttest_2 = ttest_sub(CO2_mean, CO2_std, nyears,
+                        CTRL_mean, CTRL_std, nyears) < ttest_level
+    # SRM, ctrl T-Test
+    ttest_3 = ttest_sub(SRM_mean, SRM_std, nyears,
+                        CTRL_mean, CTRL_std, nyears) < ttest_level
+    
+    # This geomip_data.py function returns dictionary of combinations of results
+    bool_dict = bools_x8_3ttests(ttest_1,ttest_2,ttest_3)
+    
+    # This geomip_data.py function returns dictionary of types of results
+    type_dict, group_dict = types_groups_from_bools(bool_dict, ratio)
+    
+    return group_dict['better_off'], group_dict['worse_off'], group_dict['dont_know']
+# End def
+
